03a7ac9c75be45b34446726f3e2ea3576cffc482
Scott <Scotthall1983@hotmail.com>
2019-10-17 10:08:33 +0100

    Removed the constructor function as no longer required to initialize state.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = { lat: null, errorMessage: '' };
 
-  }
+  state = {lat: null, errorMessage: ''};
 
   componentDidMount(){
     console.log('my component was rendered to the screen')
