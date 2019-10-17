e516221055e441ade26a86c80d4abd25e8a762e4
Scott <Scotthall1983@hotmail.com>
2019-10-17 10:49:24 +0100

    Removed the display content from the render method and replaced it with a method to call SeasonDisplay. Added a console log in SeasonDisplay to ensure that the props are being passed down to the child component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
 
-  state = {lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage:'' };
 
-  componentDidMount(){
-    console.log('my component was rendered to the screen')
+  componentDidMount() {
+    console.log('content rendered');
 
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState ({ lat: position.coords.latitude }),
+      position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
@@ -24,7 +25,7 @@ class App extends React.Component{
         return <div>Error: {this.state.errorMessage}</div>
       }
       if (!this.state.errorMessage && this.state.lat){
-        return <div>Latitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat} />
       }
     return <div>Loading!</div>;
   }
