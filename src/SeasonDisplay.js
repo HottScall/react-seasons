42c9c5d548701443e6f1c01f7d8abcb10b0f5195
Scott <Scotthall1983@hotmail.com>
2019-10-18 12:28:44 +0100

    Added a helper variable named text which is set equal to season, inside season I added a ternary express which returns it's brass monkeys or it's a scorcher depending on the location. Checked in DOM to ensure it runs on multiple locations.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,8 +11,9 @@ const getSeason = (lat, month) =>{
 
 const SeasonDisplay = props =>{
   const season = getSeason(props.lat, new Date().getMonth())
-  console.log(season);
-  return <div>Season Display</div>;
+  const text = season === 'winter' ? 'its brass monkeys' : 'its a scorcher'
+
+  return <div>{text}</div>
 };
 
 export default SeasonDisplay;
