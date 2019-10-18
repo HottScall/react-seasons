8b25a8498a419285e608aa50134368844819d937
Scott <Scotthall1983@hotmail.com>
2019-10-18 12:41:01 +0100

    Added an additional helper vairable call icon, which returns either snoflake or sun depending on the location, then in our return statement added a index tag set to icon.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,9 +11,16 @@ const getSeason = (lat, month) =>{
 
 const SeasonDisplay = props =>{
   const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'winter' ? 'its brass monkeys' : 'its a scorcher'
+  const text = season === 'winter' ? 'its brass monkeys' : 'its a scorcher';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
-  return <div><h1>{text}</h1></div>
+  return (
+  <div>
+  <i className={`${icon} icon`} />
+  <h1>{text}</h1>
+  <i className={`${icon} icon`} />
+  </div>
+  );
 };
 
 export default SeasonDisplay;
