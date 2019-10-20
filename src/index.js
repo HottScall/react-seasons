e6d2c47a68136b88988f37b1346d8c23f8ee353f
Scott <Scotthall1983@hotmail.com>
2019-10-20 08:59:23 +0100

    Removed hardcoded word loading in Spinner.js and replaced it with a prop.message call. Added a message string to the SeasonDisplay function call in Index.js. Ensured that it's working in the DOM. Changed class to className in Spinner.js to satisy an naming error which came up in the console.

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <Spinner />;
+    return <Spinner message="Please allow location services in your browser"/>;
   }
 }
 
