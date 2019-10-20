146c047cf3287d4f61884a6fb1ad064dd06720d3
Scott <Scotthall1983@hotmail.com>
2019-10-20 09:16:49 +0100

    Minor code clean up in Spinner.js

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Spinner = (props) => {
+const Spinner = props => {
   return (
     <div className="ui active dimmer">
       <div className="ui big text loader">{props.message}</div>
@@ -8,8 +8,8 @@ const Spinner = (props) => {
   );
 };
 
-Spinner.defaultProps={
-  message: 'Loading....'
+Spinner.defaultProps = {
+  message: "Loading...."
 };
 
 export default Spinner;
